bef64227b67d36ac422174032d656f626aaf74d6
Maxime YIP <maxime.yip@epita.fr>
2019-06-30 09:58:17 +0200

    [STEP 5]

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,28 +1,50 @@
 import React, { useEffect, useState, useRef } from 'react';
 import Post from './Post';
 
-const PostsList = ({ posts = [] }) => {
+const PostsList = () => {
 
-    const [data, setData] = useState([]);
+    const [posts, setPosts] = useState([]);
+
+    const titleRef = useRef(null);
+    const bodyRef = useRef(null);
+
     useEffect(() => {
-      fetch("https://jsonplaceholder.typicode.com/posts").then(response => {
-        response.json().then(data => {
-          setData(data);
+      fetch('https://jsonplaceholder.typicode.com/posts').then(res => {
+        res.json().then(data => {
+          setPosts(data);
         })
-      })
-    }, []);
+      });
+    }, [])
 
-    let nameRef = useRef();
-    let bodyRef = useRef();
+    const addPosts = (title, body) => {
+      let data = posts;
+      data.unshift({
+        title: title,
+        body: body,
+      });
+      setPosts(data);
+    }
+  
+    const handleSendClick = () => {
+      addPosts(titleRef.current.value, bodyRef.current.value);
+      titleRef.current.value = "";
+      bodyRef.current.value = "";
+    }
 
-    
-
-  return (
-  <ul style={{ listStyle: 'none', width: '800px' }}>
-    {data.map(p => (
-      <Post data={p} />
-    ))}
-  </ul>
-)};
+    return (
+      <div>
+        <center>
+          <input ref={titleRef} placeholder="Title" />
+          <input ref={bodyRef} placeholder="Body" />
+          <button onClick={handleSendClick}>send</button>
+        </center>
+        <ul style={{ listStyle: 'none', width: '800px' }}>
+          {posts.map(p => (
+            <Post data={p} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
 
 export default PostsList;
